cbf56a26b35a7e666d18a5c58b7b21329a340679
HUDannyCao <danny.cao@student.hu.nl>
2018-11-19 14:11:54 +0100

    backup oude portfolio website

---
+++ index.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, minimal-ui, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <meta name="description" content="Portfolio website van Danny Cao Webdeveloper">
+  <meta name="keywords" content="Danny, Cao, Danny Cao, Webdeveloper, programmeur, Website, Webite maker, Danny Cao Webdeveloper">
+  <meta property="og:title" content="Danny Cao - Webdeveloper">
+  <meta property="og:image" content="./images/logo.png">
+  <meta property="og:description" content="Danny Cao | Webdeveloper">
+  <meta property="og:url" content="https://dannycao.nl//">
+  <title>Danny Cao - Webdeveloper</title>
+
+  <link href="./images/css" rel="stylesheet" type="text/css">
+
+  <link rel="stylesheet" href="style.css">
+
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="js/main.js"></script> <!-- Resource jQuery -->
+
+  <link href='http://fonts.googleapis.com/css?family=Droid+Serif|Open+Sans:400,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+	<script src="js/modernizr.js"></script> <!-- Modernizr -->
+
+</head>
+
+
+<body class="non-touch ">
+
+  <header class="nav-bar">
+
+    <div class="nav-logo pull-left">
+      <a href="http://www.dannycao.nl">
+
+        <img src="./images/logo.png" alt="">
+      </a>
+
+    </div>
+
+
+    <span class="header-slogan hidden-xs pull-left">Portfolio</span>
+
+    <div class="main-nav-links hidden-sm hidden-xs">
+    <ul class="nav-links">
+      <li><a href="mailto:Danny.dc.cao@gmail.com">Contact</a></li>
+    </ul>
+    </div>
+
+  </header>
+  <div id="background-image" style="background: url(http://www.groenehart.nl/thumbs/file/510/style_discover_background/1312191457/Woerden%20Groene%20Hart%2000021.jpg) no-repeat center center; background-size: cover; -webkit-background-size: cover;-moz-background-size: cover; -o-background-size: cover;"></div>
+  <div id="background-overlay"></div>
+
+  <div class="main-content">
+
+
+  <div class="container home-content">
+    <div class="hidden-xs hidden-sm">
+      <img class="img-responsive" src="./images/me.jpg"
+      <h1>Danny Cao</h1>
+      <p>Mijn naam is Danny Cao. Ik ben 21 jaar en woon in Woerden.<br>
+         Ik studeer Media developer bij MBO Utrecht.<br>
+
+
+          Vanaf mijn basisschool had ik al veel interesse voor games en het internet. Daardoor begon ik al vroeg te experimenteren met het bouwen van games en websites.
+          Momenteel zit ik in het Examen jaar van mijn opleiding en ik heb zowel ervaring met front-end en back-end programmeren van een website.
+
+      </p>
+    </div>
+    <div id="popular-downloads">
+      <div class="row">
+        <h2><span class="icon-stars"></span>Recente Projecten</h2> </div>
+      <div class="row">
+        <div class="browse-movie-wrap col-xs-10 col-sm-5">
+          <a href="DR.html" class="browse-movie-link">
+            <figure> <img class="img-responsive" src="./images/DR1.png">
+              <figcaption class="hidden-xs hidden-sm">
+
+                <span class="button-green-download-big">Meer informatie</span> </figcaption>
+            </figure>
+          </a>
+
+        </div>
+        <div class="browse-movie-wrap col-xs-10 col-sm-5">
+          <a href="DUC.html" class="browse-movie-link">
+            <figure> <img class="img-responsive" src="./images/DUC.png">
+              <figcaption class="hidden-xs hidden-sm">
+               <span class="button-green-download-big">Meer informatie</span> </figcaption>
+            </figure>
+          </a>
+
+        </div>
+        <div class="browse-movie-wrap col-xs-10 col-sm-5">
+          <a href="RWO.html" class="browse-movie-link">
+            <figure> <img class="img-responsive" src="./images/rwologo.png">
+              <figcaption class="hidden-xs hidden-sm">
+             <span class="button-green-download-big">Meer informatie</span> </figcaption>
+            </figure>
+          </a>
+
+        </div>
+        <div class="browse-movie-wrap col-xs-10 col-sm-5">
+          <a href="NJ.html" class="browse-movie-link">
+            <figure> <img class="img-responsive" src="./images/Nje.png">
+              <figcaption class="hidden-xs hidden-sm">
+              <span class="button-green-download-big">Meer informatie</span> </figcaption>
+            </figure>
+          </a>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="content-dark">
+
+    <section id="cd-timeline" class="cd-container">
+
+      <div class="cd-timeline-block">
+
+        <div class="cd-timeline-img cd-picture">
+          <img src="img/cd-icon-location.svg" alt="Location">
+        </div> <!-- cd-timeline-img -->
+        <div class="cd-timeline-content">
+          <h2>Examenjaar MBO Utrecht </h2>
+
+          <span class="cd-date"> 2016 - 2017</span>
+
+        </div> <!-- cd-timeline-content -->
+      </div> <!-- cd-timeline-block -->
+
+
+      <div class="cd-timeline-block">
+
+        <div class="cd-timeline-img cd-picture">
+          <img src="img/cd-icon-location.svg" alt="Location">
+
+        </div> <!-- cd-timeline-img -->
+
+        <div class="cd-timeline-content">
+          <h2>Stage gelopen Bij <a href="http://www.VDMI.nl" target="_blank">VDMI Services</a></h2>
+          <p>VDMI Services is een InternetBureau in Rotterdam gespecialiceerd in DRupal CMS. Ik heb tijdens mijn stage bij VDMI veel gewerkt met Drupal 8, PHP, AngularJS.</p>
+
+          <span class="cd-date">November 2015 - April 2016</span>
+        </div> <!-- cd-timeline-content -->
+
+      </div> <!-- cd-timeline-block -->
+
+      <div class="cd-timeline-block">
+        <div class="cd-timeline-img cd-movie">
+          <img src="img/cd-icon-location.svg" alt="Location">
+        </div> <!-- cd-timeline-img -->
+
+        <div class="cd-timeline-content">
+          <h2>Stage gelopen bij <a href="http://www.designbase.nl" target="_blank">Designbase</a></h2>
+          <p>Designbase is een webbureau gespecialiseerd in Modx CMS en SEO. Ik heb tijdens mij stage bij designbase vooral gewerkt met de MODX CMS en PHP, Css3, Javascript</p>
+          <span class="cd-date">September 2014 - Februari 2015</span>
+        </div> <!-- cd-timeline-content -->
+      </div> <!-- cd-timeline-block -->
+
+      <div class="cd-timeline-block">
+        <div class="cd-timeline-img cd-picture">
+          <img src="img/cd-icon-location.svg" alt="Location">
+        </div> <!-- cd-timeline-img -->
+
+        <div class="cd-timeline-content">
+          <h2>Begonnen met de Opleiding Mediadeveloper</h2>
+          <span class="cd-date">September 2013</span>
+        </div> <!-- cd-timeline-content -->
+      </div> <!-- cd-timeline-block -->
+    </section> <!-- cd-timeline -->
+  </div>
+
+  <div class="container home-content">
+    <h2 id="skills">Vaardigheden</h2>
+    <br>
+    <p>Tijdens mijn opleiding en stages heb ik veel ervaring opgedaan met het maken van Websites. Hieronder zie je wat ik allemaal kan.</p>
+    <br>
+    <br>
+    <h2 id="skills-cont">Front-end Development</h2>
+    <div class="logos">
+    <div class="logo">
+      <ul>
+      <li><img src="./images/html5.png" alt="html5" width="74" height="84"></li>
+      <li><strong>HTML5</strong></li>
+      </ul>
+    </div>
+    <div class="logo">
+      <ul>
+      <li><img src="./images/css3.png" alt="css3" width="74" height="84"></li>
+      <li><strong>CSS3</strong></li>
+      </ul>
+    </div>
+    <div class="logo">
+      <ul>
+      <li><img src="./images/angularjs.png" alt="angular" width="74" height="84"></li>
+      <li><strong>Angular</strong></li>
+      </ul>
+    </div>
+    <div class="logo">
+      <ul>
+      <li><img src="./images/js.png" alt="js" width="74" height="84"></li>
+      <li><strong>Javascript</strong></li>
+      </ul>
+    </div>
+  </div>
+
+
+
+    <h2 id="skills-cont">Back-end Development</h2>
+      <div class="logos">
+        <div class="logo">
+          <ul>
+          <li><img src="./images/php1.png" alt="PHP" width="100" height="84"></li>
+          <li><strong>PHP</strong></li>
+        </ul>
+        </div>
+      </div>
+
+
+    <h2 id="skills-cont">Frameworks</h2>
+        <div class="logos">
+          <div class="logo">
+            <ul>
+            <li><img src="./images/laravel.png" alt="PHP" width="100" height="84"></li>
+            <li><strong>Laravel</strong></li>
+            </ul>
+          </div>
+          <div class="logo">
+            <ul>
+            <li><img src="./images/symfony.svg" alt="Symfony" width="100" height="84"></li>
+            <li><strong>Symfony</strong></li>
+            </ul>
+          </div>
+        </div>
+    <h2 id="skills-cont">CMS</h2>
+    <div class="logos">
+      <div class="logo">
+        <ul>
+        <li><img src="./images/wordpress.png" alt="Wordress" width="100" height="84"></li>
+        <li><strong>Wordpress</strong></li>
+        </ul>
+      </div>
+      <div class="logo">
+        <ul>
+        <li><img src="./images/drupal8.png" alt="drupal" width="100" height="84"></li>
+        <li><strong>Drupal</strong></li>
+        </ul>
+      </div>
+      <div class="logo">
+        <ul>
+        <li><img src="./images/modx.png" alt="modx" width="100" height="84"></li>
+        <li><strong>Modx</strong></li>
+        </ul>
+      </div>
+    </div>
+    <h2 id="skills-cont">Editing Tools</h2>
+      <div class="logos">
+        <div class="logo">
+          <ul>
+          <li><img src="./images/gimp.jpg" alt="Gimp" width="100" height="84"></li>
+          <li><strong>Gimp</strong></li>
+          </ul>
+        </div>
+      </div>
+
+  </div>
+
+
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-20">
+          <ul class="text-center">
+            <li>© Danny Cao</li>
+            <li>-</li>
+            <li><a href="CV.pdf">Download CV</a></li>
+            <li>-</li>
+            <li><a href="mailto:Danny.dc.cao@gmail.com">Danny.dc.cao@gmail.com</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+
+      </div>
+    </div>
+  </footer>
+  </div>
+
+
+
+
+
+</body>
+
+</html>
